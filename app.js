6a4dc27df5b61da2da5ea1d8dd7676f3f893b1ca
Vincent Caronnet <120@120.design>
2021-05-28 20:56:01 +0200

    Debug Heroku

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { PORT, AUTHORIZED_KEYS } = process.env
 const port = PORT || 3000
 
 global.authorizedKeys = AUTHORIZED_KEYS.split(' ')
+console.log('global.authorizedKeys', global.authorizedKeys)
 
 const express = require('express')
 const helmet = require('helmet')
@@ -15,9 +16,9 @@ const app = express()
 app.use(helmet())
 
 if (process.env.NODE_ENV !== 'production') {
-  const expressEnforcesSSL = require('express-enforces-ssl')
-  app.enable('trust proxy')
-  app.use(expressEnforcesSSL())
+  // const expressEnforcesSSL = require('express-enforces-ssl')
+  // app.enable('trust proxy')
+  // app.use(expressEnforcesSSL())
 }
 
 app.use(express.json())
